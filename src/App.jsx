4fcf0e6d56baf4874e7254bec3e59cc381f7aea6
chaitu426 <interiormern@gmail.com>
2025-01-18 15:51:54 +0530

    commited

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/hero'
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    </>
+  )
+}
+
+export default App
